5ceef5d3e653d28e7806617e4fdf1667d32c20d7
Sorathiya Nabhag <nabhagsorathiya81@gmail.com>
2026-01-06 11:16:48 +0530

    Update books.model.js

--- expressJS/Library management system/models/books.model.js
+++ expressJS/Library management system/models/books.model.js
@@ -1,9 +1,27 @@
 const { Timestamp } = require('bson')
 const mongoose = require('mongoose')
 
-const userSchema = mongoose.Schema({
-        bookName: String,
-        bookId: Number,
-}, {Timestamp:true})
+const bookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: String,
 
-module.exports = mongoose.model("books", userSchema)
+    totalQuantity: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+
+    availableQuantity: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Book", bookSchema);
