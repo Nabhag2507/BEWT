5567133b898944f6db6a3fc97b3604298d038dd6
Nabhag2507 <nabhagsorathiya81@gmail.com>
2026-01-27 11:27:55 +0530

    Initialize Next.js project with essential configuration and structure

---
+++ nextJS/first-app/src/app/about/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AboutPage() {
+    return <h1>This is about page</h1>;
+}
+
+export default AboutPage;
---
+++ nextJS/first-app/src/app/contact/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ContactPage() {
+    return <h1>This is our contact page</h1>;
+}
+
+export default ContactPage;
